b3693c865c520832526b802ee42e5df9bfa2071e
Lorenz <lorenzgillesania20@gmail.com>
2023-05-30 21:22:09 +0800

    modified 'authApiSlice.js'

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,3 +1,33 @@
 import { api } from './../api/apiSlice';
 const authApiRoute = 'auth';
-const authApi = api.injectEndpoints({})
+const authApi = api.injectEndpoints({
+    endpoints: (build) => ({
+        login: build.mutation({
+            query: ({username, password}) => ({
+                url: `${authApiRoute}/login`,
+                method: 'POST',
+                body: {
+                    username,
+                    password
+                }
+            })
+        }),
+        refresh: build.mutation({
+            query: () => ({
+                url: `${authApiRoute}/refresh`,
+                method: 'POST'
+            })
+        }),
+        logout: build.mutation({
+            query: () => ({
+                url: `${authApiRoute}/logout`,
+                method: 'POST'
+            })
+        })
+    })
+})
+export const {
+    useLoginMutation,
+    useRefreshMutation,
+    useLogoutMutation
+} = authApi;
