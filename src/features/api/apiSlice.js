f7ff2ebc81904113ef06ade37660a1d977131293
Lorenz <lorenzgillesania20@gmail.com>
2023-05-31 15:11:25 +0800

    modified 'apiSlice.js'

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,19 @@ import {
     fetchBaseQuery
 } from '@reduxjs/toolkit/query/react';
 const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_URL,
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().token;
+            if(
+                typeof token === 'string'
+                || token.length > 0
+            ){
+                headers.set('authorization', `Bearer ${token}`);
+            }
+            return headers;
+        }
+    }),
     tagTypes: ['Users', 'Tasks'],
     endpoints: () => ({})
 });
