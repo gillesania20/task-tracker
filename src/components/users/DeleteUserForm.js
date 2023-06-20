4c0edf034986933667b91128ef23e4a56cbb62e0
Lorenz <lorenzgillesania20@gmail.com>
2023-06-20 18:03:02 +0800

    modified 'DeleteUserForm.js'

--- src/components/users/DeleteUserForm.js
+++ src/components/users/DeleteUserForm.js
@@ -1,12 +1,20 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useDeleteUserMutation } from './../../features/users/userApiSlice';
 import { useRefreshMutation } from './../../features/auth/authApiSlice';
+import { selectRole } from './../../features/auth/authSlice';
 const DeleteUserForm = () => {
     const { userId } = useParams();
     const [message, setMessage] = useState('');
     const [deleteUser, { isLoading }] = useDeleteUserMutation();
     const [refresh, { isLoading: isLoadingRefresh }] = useRefreshMutation();
+    const navigate = useNavigate();
+    const role = useSelector(selectRole);
+    const onClickCancel = () => {
+        navigate(`/dash/users/display-user/${userId}`);
+        return null;
+    }
     const handleSubmit = async (e) => {
         e.preventDefault();
         const refreshResponse = await refresh();
@@ -14,6 +22,11 @@ const DeleteUserForm = () => {
             const response = await deleteUser({userId});
             if(response.data?.message === 'user deleted'){
                 setMessage(response.data.message);
+                if(role === 'Admin'){
+                    navigate('/dash/users/display-all-users');
+                }else{
+                    navigate('/login');
+                }
             }else if(typeof response.error?.data?.message !== 'undefined'){
                 setMessage(response.error.data.message);
             }else{
@@ -37,7 +50,11 @@ const DeleteUserForm = () => {
                 <div>LOADING...</div>
                 :<button type='submit'>Delete</button>
             }
-            <button type='button'>Cancel</button>
+            <button type='button'
+                onClick={onClickCancel}
+            >
+                Cancel
+            </button>
         </div>
     </form>;
     return content;
